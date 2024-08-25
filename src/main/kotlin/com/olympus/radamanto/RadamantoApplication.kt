abbd44c4e6c2c610d8b5f5d0c6a31c7b0c802dc4
Harlan Cleiton <harlancleiton@gmail.com>
2024-08-25 08:27:23 -0300

    Initial commit

---
+++ src/main/kotlin/com/olympus/radamanto/RadamantoApplication.kt
@@ -0,0 +1,11 @@
+package com.olympus.radamanto
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RadamantoApplication
+
+fun main(args: Array<String>) {
+	runApplication<RadamantoApplication>(*args)
+}
---
+++ src/test/kotlin/com/olympus/radamanto/RadamantoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.olympus.radamanto
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RadamantoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
